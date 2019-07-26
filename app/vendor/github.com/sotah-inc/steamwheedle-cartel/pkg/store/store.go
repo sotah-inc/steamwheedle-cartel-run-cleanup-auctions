ae16039947f9b4f77e637a1c2d075b599e009b2b
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-26 14:10:30 -0700

    Pull in run-cleanup-auctions functionality.

---
+++ app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/store/store.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"context"
+
+	"cloud.google.com/go/storage"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+)
+
+func NewClient(projectID string) (Client, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return Client{}, err
+	}
+
+	s := Client{Context: ctx, projectID: projectID, client: client}
+
+	return s, nil
+}
+
+type Client struct {
+	Context   context.Context
+	projectID string
+	client    *storage.Client
+}
+
+type DeleteAllFromTimestampsJob struct {
+	sotah.RegionRealmTimestampTuple
+	Err error
+}
